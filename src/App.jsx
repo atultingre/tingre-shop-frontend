01b81090ccdbdc4b5124b05b5c0ecfc2d4a90c9b
Atul Tingre <atultingre.work@gmail.com>
2024-04-30 13:57:40 +0530

    Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Auth/Login/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import ProductForm from "./components/Admin/Product/ProductForm";
 import ProductList from "./components/Admin/Product/ProductList";
 import Register from "./components/Auth/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
+import NotFound from "./components/NotFound/NotFound";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
   return (
@@ -12,8 +15,11 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/add" element={<ProductForm />} />
-        <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add" element={<ProductForm />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer />
     </div>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+
+const Layout = () => {
+  return (
+    <>
+      <Dashboard />
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
