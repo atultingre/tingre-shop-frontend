c03e800f16721c553f93dfccc6ac613f37308352
Atul Tingre <atultingre.work@gmail.com>
2024-05-01 17:21:10 +0530

    css update

--- src/components/Layout/Dashboard/Dashboard.jsx
+++ src/components/Layout/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import Header from "../Header/Header";
 const Dashboard = ({ children }) => {
   return (
     <>
-      <div className="min-h-full">
+      <div className="min-h-full mt-12">
         <Header />
         {children}
       </div>
