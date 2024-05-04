c6539d0c9495b981c864ebd91c321141297182e7
Atul Tingre <atultingre.work@gmail.com>
2024-05-04 14:34:22 +0530

    Success page added

--- src/components/frontend/PlaceOrder/OrderSuccess.jsx
+++ src/components/frontend/PlaceOrder/OrderSuccess.jsx
@@ -1,6 +1,28 @@
+import Lottie from "lottie-react";
+import success from "./success.json";
+import { useStore } from "../../../context/StoreContext";
 
 const OrderSuccess = () => {
-  return <div>OrderSuccess</div>;
+  const { navigate } = useStore();
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <Lottie
+        className="min-w-[400px]"
+        loop={true}
+        autoplay={true}
+        animationData={success}
+      />
+      <h2 className="text-3xl font-bold mt-4 text-center">
+        Order Placed Successfully
+      </h2>
+      <button
+        onClick={() => navigate("/myorders")}
+        className="mt-8 px-6 py-3 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300 ease-in-out"
+      >
+        Go to Orders
+      </button>
+    </div>
+  );
 };
 
 export default OrderSuccess;
